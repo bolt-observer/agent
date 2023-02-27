9f908e3aaae61747833d1d600f082a376f3dac98
Gregor Pogacnik <gregor@bolt.observer>
2023-02-27 15:29:23 +0100

    fix: ln cleanup should not be always invoked

--- lightning/api_clncommando_test.go
+++ lightning/api_clncommando_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	entities "github.com/bolt-observer/go_common/entities"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,4 +28,25 @@ func TestGetViaCommando(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, true, full.Node.Alias == info.Alias)
+}
+
+func TestFailureToConnect(t *testing.T) {
+
+	// We don't want to connect to random IPs from CI, this actually crashed before the fix w/ api.Cleanup
+	return
+
+	api := NewAPI(ClnCommando, func() (*entities.Data, error) {
+		return &entities.Data{
+			PubKey:      "02bf4f3f91e4bad02fae3a6a55d711d41d62f317ca1c76294f76612bae4b102ad2",
+			MacaroonHex: "SzPwo1iVvWaaaz_FR1z7Ds9PKo23P4zVkLjcNJb98to9MSZtZXRob2RebGlzdHxtZXRob2ReZ2V0fG1ldGhvZD1zdW1tYXJ5Jm1ldGhvZC9saXN0ZGF0YXN0b3Jl",
+			Endpoint:    "1.2.3.4:9735",
+		}, nil
+	})
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(15*time.Second))
+	defer cancel()
+	api.GetInfo(ctx)
+
+	api.Cleanup()
+
 }
