608f72b7209f6ddf499d0621bd169372c4f16dbd
Gregor Pogacnik <gregor@bolt.observer>
2023-03-16 14:24:45 +0100

    test: fix nodedata tests

--- plugins/boltz/db_test.go
+++ plugins/boltz/db_test.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -14,6 +15,11 @@ const dbpath = "./test.db"
 
 type Dummy struct {
 	Name string
+}
+
+type ExtendedJobData struct {
+	Burek string
+	JobData
 }
 
 func TestBoltzDB(t *testing.T) {
@@ -45,5 +51,23 @@ func TestBoltzDB(t *testing.T) {
 		var d Dummy
 		err := db.Get("notfound", &d)
 		require.EqualError(t, err, "No data found for this key")
+	})
+	t.Run("Extended entities", func(t *testing.T) {
+		jd := &JobData{ID: 1337}
+		err = db.Insert(1337, jd)
+		assert.NoError(t, err)
+
+		var jd2 JobData
+		err = db.Get(1337, &jd2)
+		assert.NoError(t, err)
+
+		assert.Equal(t, int32(1337), jd.ID)
+
+		ejd := &ExtendedJobData{Burek: "mesni", JobData: *jd}
+		err = db.Insert(1337, ejd)
+		assert.NoError(t, err)
+
+		//err = db.Get(1337, &jd2)
+		//assert.NoError(t, err)
 	})
 }
