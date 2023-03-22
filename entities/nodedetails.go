de6ba4947030a22010f7a9d26df8844eab45e19a
Gregor Pogacnik <gregor@bolt.observer>
2023-03-22 12:58:55 +0100

    feat: confirmed/unconfirmed balance, get rid of obsolete monitoring copy/paste

--- entities/nodedetails.go
+++ entities/nodedetails.go
@@ -6,11 +6,12 @@ import (
 
 // NodeDetails struct
 type NodeDetails struct {
-	NodeVersion         string `json:"node_version"`
-	IsSyncedToChain     bool   `json:"is_synced_to_chain"`
-	IsSyncedToGraph     bool   `json:"is_synced_to_graph"`
-	AgentVersion        string `json:"agent_version"` // will be filled before sending
-	TotalOnChainBalance uint64 `json:"total_onchain_balance"`
+	NodeVersion                    string `json:"node_version"`
+	IsSyncedToChain                bool   `json:"is_synced_to_chain"`
+	IsSyncedToGraph                bool   `json:"is_synced_to_graph"`
+	AgentVersion                   string `json:"agent_version"` // will be filled before sending
+	TotalOnChainBalanceConfirmed   uint64 `json:"total_onchain_balance_confirmed"`
+	TotalOnChainBalanceUnconfirmed uint64 `json:"total_onchain_balance_unconfirmed"`
 
 	api.NodeInfoAPIExtended
 }
